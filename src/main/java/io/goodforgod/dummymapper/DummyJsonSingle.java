f32752e812bd44a854ce6ef50fbc2c9ebe4fdbe4
Anton Kurako <goodforgod.dev@gmail.com>
2019-11-17 14:40:20 +0300

    Initial dummy mapper action setup

---
+++ src/main/java/io/goodforgod/dummymapper/DummyJsonSingle.java
@@ -0,0 +1,19 @@
+package io.goodforgod.dummymapper;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+
+/**
+ * ! NO DESCRIPTION !
+ *
+ * @author GoodforGod
+ * @since 17.11.2019
+ */
+public class DummyJsonSingle extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
