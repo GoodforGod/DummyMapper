cd35e737b3a705c2fa603ee6aaa759c29b243cad
Anton Kurako <goodforgod.dev@gmail.com>
2019-11-21 20:50:56 +0300

    Some setup for file finding

--- src/main/java/io/goodforgod/dummymapper/DummyJsonSingle.java
+++ src/main/java/io/goodforgod/dummymapper/DummyJsonSingle.java
@@ -2,6 +2,15 @@ package io.goodforgod.dummymapper;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+
+import java.net.URL;
+import java.net.URLClassLoader;
 
 
 /**
@@ -14,6 +23,22 @@ public class DummyJsonSingle extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        final PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        final Project project = e.getProject();
+        final PsiElement elementAt = psiFile.findElementAt(editor.getCaretModel().getOffset());
+        try {
+            final PsiDirectory directory = elementAt.getContainingFile().getContainingDirectory();
+            final String dirPath = directory.toString().replace("PsiDirectory:", "file:/");
+            final String targetName = elementAt.getContainingFile().getVirtualFile().getName();
+            final String target = elementAt.getContainingFile().getVirtualFile().getUrl();
+            final URL url = new URL(dirPath);
+            final URLClassLoader loader = new URLClassLoader(new URL[]{url});
+            Class<?> aClass = loader.loadClass(targetName);
+            aClass.toString();
+            //TODO dynamically find, compile, load class (and his parents)
+        } catch (Exception ex) {
+            return;
+        }
     }
 }
