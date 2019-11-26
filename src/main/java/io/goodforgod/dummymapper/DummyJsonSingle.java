c9e15db9b6ca734d0fa1d73f17555fa0ec975bcf
Anton Kurako <goodforgod.dev@gmail.com>
2019-11-26 23:52:38 +0300

    Setup for recursive field scanning

--- src/main/java/io/goodforgod/dummymapper/DummyJsonSingle.java
+++ src/main/java/io/goodforgod/dummymapper/DummyJsonSingle.java
@@ -5,12 +5,14 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -25,6 +27,9 @@ public class DummyJsonSingle extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
         final PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        PsiFieldVisitor visitor = new PsiFieldVisitor();
+        psiFile.accept(visitor);
+        Map<String, Object> map = visitor.getMap();
         final Project project = e.getProject();
         final PsiElement elementAt = psiFile.findElementAt(editor.getCaretModel().getOffset());
         try {
@@ -40,5 +45,26 @@ public class DummyJsonSingle extends AnAction {
         } catch (Exception ex) {
             return;
         }
+    }
+
+    private Map<String, Object> getClassMap(PsiFile file) {
+        final PsiClass[] classes = ((PsiJavaFile) file).getClasses();
+        final Map<String, Object> map = new HashMap<>();
+        for (PsiClass psiClass : classes) {
+            for (PsiField field : psiClass.getAllFields()) {
+                if(isFieldSimple(field)) {
+                    map.put(field.getName(), field.getType().getCanonicalText());
+                } else {
+                    final VirtualFile subTypeFile = field.getType().getResolveScope().getProject().getProjectFile();
+
+                }
+            }
+        }
+
+        return Collections.emptyMap();
+    }
+
+    private boolean isFieldSimple(PsiField field) {
+        return true;
     }
 }
---
+++ src/main/java/io/goodforgod/dummymapper/PsiFieldVisitor.java
@@ -0,0 +1,85 @@
+package io.goodforgod.dummymapper;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+
+import java.util.*;
+
+/**
+ * ! NO DESCRIPTION !
+ *
+ * @author GoodforGod
+ * @since 26.11.2019
+ */
+public class PsiFieldVisitor extends PsiRecursiveElementVisitor {
+
+    private final Map<String, Object> map = new HashMap<>();
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    @Override
+    public void visitFile(PsiFile file) {
+        super.visitFile(file);
+    }
+
+    @Override
+    public void visitElement(PsiElement element) {
+        if (element instanceof PsiField) {
+            final PsiField field = (PsiField) element;
+            if (isFieldSimple(field)) {
+                map.put(field.getName(), field.getType().getCanonicalText());
+            } else {
+                Project project = field.getType().getResolveScope().getProject();
+                if (project != null) {
+                    PsiFile[] filesByName = FilenameIndex.getFilesByName(project,
+                            field.getType().getCanonicalText() + ".class",
+                            GlobalSearchScope.allScope(project));
+
+                    PsiFieldVisitor psiFieldVisitor1 = new PsiFieldVisitor();
+                    PsiField[] fields = ((PsiJavaFile) filesByName[0]).getClasses()[0].getAllFields();
+
+                    if (filesByName.length > 0) {
+                        PsiFieldVisitor psiFieldVisitor = new PsiFieldVisitor();
+                        filesByName[0].accept(psiFieldVisitor);
+                        map.put(field.getName(), psiFieldVisitor.getMap());
+                    }
+                }
+            }
+        }
+
+        super.visitElement(element);
+    }
+
+    private boolean isFieldSimple(PsiField field) {
+        final String canonicalText = field.getType().getCanonicalText();
+        List<String> strings = Arrays.asList(
+                "String",
+                "Integer",
+                "Byte",
+                "Short",
+                "Integer",
+                "Long",
+                "Float",
+                "Double",
+                "Boolean",
+                "Character",
+                "byte",
+                "int",
+                "long",
+                "float",
+                "double",
+                "boolean",
+                "char",
+                "Object",
+                "BigInteger",
+                "BigNumber"
+        );
+
+        HashSet<String> set = new HashSet<>(strings);
+        return set.contains(canonicalText);
+    }
+}
