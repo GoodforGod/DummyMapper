a0c9f0a35d57a681ddce31b61a64fe9ad02d64ed
Anton Kurako <goodforgod.dev@gmail.com>
2019-11-29 23:50:52 +0300

    Enum field array parsing added

--- src/main/java/io/goodforgod/dummymapper/PsiJavaFileScanner.java
+++ src/main/java/io/goodforgod/dummymapper/PsiJavaFileScanner.java
@@ -3,6 +3,7 @@ package io.goodforgod.dummymapper;
 import com.intellij.lang.jvm.JvmModifier;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiType;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 
@@ -28,13 +29,17 @@ public class PsiJavaFileScanner {
 
     private final Map<String, Map> scanned = new HashMap<>();
 
+    @SuppressWarnings("unchecked")
     public Map<String, Object> scan(PsiJavaFile file) {
         final Map<String, Object> structure = new HashMap<>();
         scanned.put(getFullName(file), structure);
         if (file.getClasses().length > 0) {
             final PsiField[] fields = file.getClasses()[0].getAllFields();
             for (PsiField field : fields) {
-                if (isFieldSimple(field) && isFieldValid(field)) {
+                if (isFieldEnum(field)) {
+                    ((List) structure.computeIfAbsent(field.getType().getPresentableText(), k -> new ArrayList<String>()))
+                            .add(field.getName());
+                } else if (isFieldSimple(field) && isFieldValid(field)) {
                     structure.put(field.getName(), SIMPLE_FIELDS.get(field.getType().getCanonicalText()));
                 } else {
                     Optional.ofNullable(field.getType().getResolveScope())
@@ -47,7 +52,13 @@ public class PsiJavaFileScanner {
                             .ifPresent(f -> {
                                 final Map map = scanned.get(getFullName(f));
                                 if (map == null) {
-                                    structure.put(field.getName(), scan(f));
+                                    final Map<String, Object> scanned = scan(f);
+                                    final Object values = scanned.get(field.getType().getPresentableText());
+                                    if (values instanceof Collection) {
+                                        structure.put(field.getName(), values);
+                                    } else {
+                                        structure.put(field.getName(), scanned);
+                                    }
                                 } else {
                                     structure.put(field.getName(), map);
                                 }
@@ -61,6 +72,11 @@ public class PsiJavaFileScanner {
 
     private static String getFullName(PsiJavaFile file) {
         return file.getPackageName() + "." + file.getName();
+    }
+
+    private static boolean isFieldEnum(PsiField field) {
+        final PsiType[] superTypes = field.getType().getSuperTypes();
+        return superTypes.length > 0 && "java.lang.Enum".equals(superTypes[0].getCanonicalText());
     }
 
     private static boolean isFieldSimple(PsiField field) {
