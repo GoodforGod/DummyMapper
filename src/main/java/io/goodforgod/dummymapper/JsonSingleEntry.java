c50f8702737358dd69ac31f633e3dd1da4df6695
Anton Kurako <goodforgod.dev@gmail.com>
2020-04-21 21:43:39 +0300

    DummyMaker library 2.1.0 upgraded

--- src/main/java/io/goodforgod/dummymapper/JsonSingleEntry.java
+++ src/main/java/io/goodforgod/dummymapper/JsonSingleEntry.java
@@ -3,8 +3,6 @@ package io.goodforgod.dummymapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ex.ClipboardUtil;
-import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiJavaFile;
@@ -13,14 +11,7 @@ import io.goodforgod.dummymapper.service.ClassFactory;
 import io.goodforgod.dummymapper.service.GenFactoryBuilder;
 import io.goodforgod.dummymapper.service.JavaFileScanner;
 import io.goodforgod.dummymapper.util.IdeaUtils;
-import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -38,10 +29,9 @@ public class JsonSingleEntry extends AnAction {
             final PsiJavaFile file = IdeaUtils.getFileFromAction(event)
                     .orElseThrow(() -> new IllegalArgumentException("File is not Java File!"));
 
-            final JavaFileScanner scanner = new JavaFileScanner();
-            final Map<String, Object> scan = scanner.scan(file);
-
+            final Map<String, Object> scan = new JavaFileScanner().scan(file);
             final Class target = ClassFactory.build(scan);
+
             final GenFactory factory = GenFactoryBuilder.build(target, scan);
             final Object o = factory.build(target);
 
@@ -49,7 +39,7 @@ public class JsonSingleEntry extends AnAction {
             IdeaUtils.copyToClipboard(json);
         } catch (Exception e) {
             e.printStackTrace();
-            Messages.showMessageDialog(currentProject, e.getMessage(), "Problem Occurred", Messages.getInformationIcon());
+            Messages.showErrorDialog(currentProject, e.getMessage(), "Problem Occurred");
         }
     }
 }
