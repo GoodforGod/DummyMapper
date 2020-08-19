ef969830f585e2a9d7f2a58dcac184baffa4a93f
Anton Kurako <goodforgod.dev@gmail.com>
2020-08-19 11:25:08 +0300

    [1.0.0] Vendor URL changed MapperAction issue link as text

--- src/main/java/io/goodforgod/dummymapper/MapperAction.java
+++ src/main/java/io/goodforgod/dummymapper/MapperAction.java
@@ -37,6 +37,7 @@ import java.util.StringJoiner;
 public abstract class MapperAction<T extends IConfig> extends AnAction {
 
     private static final String DISPLAY_GROUP_ERROR = "DummyMapping Plugin Errors";
+    private static final String ISSUE_URL = "https://github.com/GoodforGod/DummyMapper/issues";
 
     public MapperAction() {
         super();
@@ -116,7 +117,7 @@ public abstract class MapperAction<T extends IConfig> extends AnAction {
         } catch (Exception e) {
             final StringJoiner joiner = new StringJoiner("\n");
             joiner.add("There was an error mapping file to " + format() + ".");
-            joiner.add("Please report <b><a href=\"https://github.com/GoodforGod/DummyMapper/issues\">this issue here</a></b>.");
+            joiner.add("Please report this issue here: <b><a href=\"" + ISSUE_URL + "\">" + ISSUE_URL + "</a></b>");
             joiner.add("Stacktrace: " + getStackTrace(e));
 
             final String title = "Failed mapping to " + format();
