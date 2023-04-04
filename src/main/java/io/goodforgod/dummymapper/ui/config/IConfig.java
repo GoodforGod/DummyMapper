ca8027dec64cc176a120b774b1d6edd8529c4416
Anton Kurako <goodforgod.dev@gmail.com>
2023-04-04 23:50:20 +0300

    [2.1.0] Idea 2023.1 support

--- src/main/java/io/goodforgod/dummymapper/ui/config/IConfig.java
+++ src/main/java/io/goodforgod/dummymapper/ui/config/IConfig.java
@@ -1,9 +1,9 @@
 package io.goodforgod.dummymapper.ui.config;
 
 import java.util.Collection;
-import javax.annotation.Nullable;
 import javax.swing.*;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Anton Kurako (GoodforGod)
